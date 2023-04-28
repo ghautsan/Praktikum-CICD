3d88b7e054d15dabcaea29f2cde81079b5638305
Rifki Ghautsan Rachman <ghautsan_r@yahoo.com>
2023-04-28 14:10:04 +0700

    update

--- src/test/java/starter/stepdefinitions/UserSteps.java
+++ src/test/java/starter/stepdefinitions/UserSteps.java
@@ -5,11 +5,15 @@ import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.And;
 import net.thucydides.core.annotations.Steps;
+import starter.user.LoginPost;
 import starter.user.RegisterPost;
 
 public class UserSteps {
     @Steps
     RegisterPost registerPost;
+
+    @Steps
+    LoginPost loginPost;
 
     @Given("I set POST api endpoints")
     public void setPostApiEndpoints() {
@@ -29,5 +33,25 @@ public class UserSteps {
     @And("I receive valid data for new user")
     public void validateDataNewUser() {
         registerPost.validateDataNewUser();
+    }
+
+    @Given("I set POST api endpoints to login")
+    public void setPostApiEndpointsToLogin() {
+        loginPost.setPostApiEndpointToLogin();
+    }
+
+    @When("I send POST HTTP request to login")
+    public void sendPostHttpRequestToLogin() {
+        loginPost.sendPostHttpRequestToLogin();
+    }
+
+    @Then("I receive valid HTTP response code 200 to login")
+    public void receiveValidHttp200ToLogin() {
+        loginPost.receiveHttpResponseCode200ToLogin();
+    }
+
+    @And("I receive valid login data")
+    public void validateLoginData() {
+        loginPost.validateLoginData();
     }
 }
--- src/test/resources/api/User.feature
+++ src/test/resources/api/User.feature
@@ -8,3 +8,9 @@ Feature: User
     When I send POST HTTP request
     Then I receive valid HTTP response code 200
     And I receive valid data for new user
+
+  Scenario: POST - As user I have be able to login
+    Given I set POST api endpoints to login
+    When I send POST HTTP request to login
+    Then I receive valid HTTP response code 200 to login
+    And I receive valid login data
